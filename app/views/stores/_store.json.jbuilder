dc90db83fb8cc8084054ef92c344bb9d0f6d3507
Mostafa Mahmoud <mahmodmostafa028@gmail.com>
2019-04-27 17:07:23 +0200

    Scaffold Store

---
+++ app/views/stores/_store.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! store, :id, :user_id, :name, :summary, :created_at, :updated_at
+json.url store_url(store, format: :json)
---
+++ db/migrate/20190427150656_create_stores.rb
@@ -0,0 +1,11 @@
+class CreateStores < ActiveRecord::Migration[5.2]
+  def change
+    create_table :stores do |t|
+      t.belongs_to :user, foreign_key: true
+      t.string :name
+      t.text :summary
+
+      t.timestamps
+    end
+  end
+end
