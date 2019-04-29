badac07de217b6f3277fa857d29d5afd204fc4bc
mariam-egm <mariam.gerges014@gmail.com>
2019-04-29 15:08:00 +0200

    Add last 5 products as new products in products views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,6 +6,7 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
     @categories = Category.all
+    @new_products = Product.last(5)
   end
 
   # GET /products/1
