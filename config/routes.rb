796576ed080870bf496f11a810e73310111b46ce
mariam-egm <mariam.gerges014@gmail.com>
2019-04-29 13:47:38 +0200

    Put products as the root

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   resources :order_products
   resources :user_coupons
   resources :orders
-  resources :products
   resources :coupons
   resources :stores
   resources :categories
   resources :brands
   devise_for :users
+  root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
