cac898727248c8be7a9a66b087c745a3b5ee0da1
mariam-egm <mariam.gerges014@gmail.com>
2019-05-03 01:53:11 +0200

    Add active storage gem and apply it on products

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,8 @@ class Product < ApplicationRecord
   belongs_to :category
   belongs_to :store
 
+  has_one_attached :image
+
   def self.search(params)
     products = Product.where("title LIKE? or description LIKE?", "%#{params[:search]}%",
                "%#{params[:search]}%") if params[:search].present?
