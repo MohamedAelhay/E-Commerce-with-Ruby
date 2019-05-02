96e58fef51b856c6229b5956e053ee565cb5c7a9
Mostafa Mahmoud <mahmodmostafa028@gmail.com>
2019-05-02 22:47:49 +0200

    Rearrange js files  requiring

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,13 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require jquery
+
 //= require popper
-//= require jquery_ujs
+
 //= require bootstrap
 //= require_tree .
 
