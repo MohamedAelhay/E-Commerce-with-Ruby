f0f6c2f237ae17513d18a47c28419b17c99dea58
Mostafa Mahmoud <mahmodmostafa028@gmail.com>
2019-05-02 21:59:01 +0200

    Install active admin gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :rate_reviews
   resources :order_products
-  resources :user_coupons
   resources :orders
   resources :products
-  resources :coupons
-  resources :stores
-  resources :categories
-  resources :brands
+  namespace :admin do
+    resources :coupons
+    resources :stores
+    resources :categories
+    resources :brands
+    resources :user_coupons
+  end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
