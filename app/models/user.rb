1a080c9f7ac277bbc5b7ba8fb11c3360e3c40da4
mariam-egm <mariam.gerges014@gmail.com>
2019-05-03 02:28:57 +0200

    Add users' avatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   # attr_accessor :username
-
+  has_one_attached :image
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
