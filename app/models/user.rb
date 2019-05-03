cf2a068c223c1fe7f08eccaceb321a1e42847f1c
YousraEssam <yousraa.essam.15@hotmail.com>
2019-05-03 16:51:24 +0200

    add reviews and rate to single product page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   # attr_accessor :username
+  has_many :rate_reviews
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
