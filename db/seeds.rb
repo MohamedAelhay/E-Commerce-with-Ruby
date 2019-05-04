b278555fa36680b00bc8f6b81e4dae329b0a01a0
Mostafa Mahmoud <mahmodmostafa028@gmail.com>
2019-05-04 18:30:47 +0200

    Remove role type from user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
-User.create!(email: 'user@example.com', password: 'passworduser', password_confirmation: 'passworduser',username: 'user',role_type: 'user') if Rails.env.development?
+User.create!(email: 'user@example.com', password: 'passworduser', password_confirmation: 'passworduser',username: 'user') if Rails.env.development?
 
  10.times do
      Brand.create([{
