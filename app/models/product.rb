87bbecbcc37f640bb01de4f185c404432b0e83e5
mariam-egm <mariam.gerges014@gmail.com>
2019-05-05 22:38:14 +0200

    Add validations to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,6 +10,15 @@ class Product < ApplicationRecord
 
   has_one_attached :image
 
+  #Validation
+  validates :brand,         presence: true
+  validates :category,      presence: true
+  validates :store,         presence: true
+  validates :title,         presence: true, length: { minimum: 3 }
+  validates :description,   presence: true, length: { minimum: 5 }
+  validates :price,         presence: true
+  validates :quantity,      presence: true
+  
   def self.search(params)
     products = Product.where("title LIKE? or description LIKE?", "%#{params[:search]}%",
                "%#{params[:search]}%") if params[:search].present?
