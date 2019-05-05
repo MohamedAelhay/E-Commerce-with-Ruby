716d2ae8ff20d5a41d4254c0beca157c92bc2819
mariam-egm <mariam.gerges014@gmail.com>
2019-05-05 23:32:30 +0200

    Add saving category test

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -4,4 +4,14 @@ class CategoryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should not save category without name" do
+    category = Category.new 
+    assert_not category.save
+  end
+  test "should save category with name" do
+    category = Category.new(name: "test")
+    assert category.save
+  end
+
 end
