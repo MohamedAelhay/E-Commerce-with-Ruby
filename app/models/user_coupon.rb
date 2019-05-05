2fbe9164a99304d2581007b1dc91ecd04753a8ce
Mostafa Mahmoud <mahmodmostafa028@gmail.com>
2019-05-05 13:57:47 +0200

    Fix user coupon validation error

--- app/models/user_coupon.rb
+++ app/models/user_coupon.rb
@@ -3,7 +3,7 @@ class UserCoupon < ApplicationRecord
   belongs_to :coupon
   def self.taken_before(user_id,code)
     coupon=Coupon.find_by(code: code)
-    userCoupon=UserCoupon.find_by("user_id = ? OR coupon_id = ?", user_id,coupon.id)
+    userCoupon=UserCoupon.find_by("user_id = ? AND coupon_id = ?", user_id,coupon.id)
     if (userCoupon.nil?)
       return false
     end
