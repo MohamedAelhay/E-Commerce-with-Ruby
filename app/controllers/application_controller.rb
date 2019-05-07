58e7b33f0b1a7c27de42d943fdfc25638e3155eb
MohamedAbdelhay <cap.mohamed.abdelhay@gmail.com>
2019-05-07 15:44:10 +0200

    Add Routes After Sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,17 @@ class ApplicationController < ActionController::Base
         format.js   { head :forbidden, content_type: 'text/html' }
       end
     end
+
+    def after_sign_in_path_for(resource)
+      if current_user.role_type == "seller"
+        store_path(current_user.store.id)
+      elsif current_user.role_type == "buyer"
+        root_path
+      else
+        root_path
+      end
+    end
+    
     protected
     def configure_permitted_parameters
     #   devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
